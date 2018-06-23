8e66ed6065a8384a14e015ede714c152f4233667
jun <ah_chong88@hotmail.com>
2018-06-23 19:24:57 +0800

    change index

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.send({ hi: 'there'});
+    res.send({ bye: 'buddy'});
 });
+
+
+//https://shielded-tundra-34868.herokuapp.com/
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
