5cc48a14c60764fae2396edd3c8b06d7f4c85cb2
jun <ah_chong88@hotmail.com>
2018-06-24 22:01:41 +0800

    enable google strategy proxy

--- services/passport.js
+++ services/passport.js
@@ -22,8 +22,10 @@ passport.use(
     new GoogleStrategy({
         clientID:  keys.googleClientId,
         clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
+        callbackURL: '/auth/google/callback',
+        proxy: true
+    }, 
+    (accessToken, refreshToken, profile, done) => {
         Users.findOne({ googleId: profile.id })
             .then(existingUser =>{
                 if(existingUser){
